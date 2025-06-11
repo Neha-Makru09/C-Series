c58d479f00597b7c16553fa494a3b3d70bb81a1f
Neha Makru <darkmelody04@gmail.com>
2025-06-11 13:46:32 +0530

    Create Pattern_15.cpp

---
+++ Pattern_15.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std; 
+
+int main(){ 
+    
+    int n; 
+    cout<<"Enter the size :"<<endl; 
+    cin>>n; 
+    cout<<endl; 
+    for (int i=1;i<=n;i++){
+        for (int j=n;j>=i;j--){ 
+           cout<<j<<" "; 
+    }
+    cout<<endl; 
+}
+    return 0;
+} 
