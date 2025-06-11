f8f9de77ffe91f6387ae71862c02bde9deda5efc
Neha Makru <darkmelody04@gmail.com>
2025-06-11 14:21:12 +0530

    Create Pattern_26.cpp

---
+++ Pattern_26.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std; 
+
+int main(){ 
+    
+    int n; 
+    cout<<"Enter the size :"<<endl; 
+    cin>>n; 
+    int a=1; 
+    cout<<endl; 
+    for (int i=1;i<=n;i++){
+        for (int j=1;j<=n;j++){ 
+           if (i==3 || j==3)
+           cout<<"* "; 
+           else 
+           cout<<"  "; 
+    }
+    cout<<endl; 
+}
+    return 0;
+} 
